baa7fd68b9e7e9c0e2b2f0293af045b5bee92780
MrWouterNL <mc1@arcadedev.nl>
2018-06-16 12:00:43 +0200

    Improved Message coloring

--- src/nl/jerskisnow/ssgminetopiaaddons/listener/BlockPlace.java
+++ src/nl/jerskisnow/ssgminetopiaaddons/listener/BlockPlace.java
@@ -11,22 +11,12 @@ public class BlockPlace implements Listener {
 	
 	private Main main = Main.getPlugin(Main.class);
 	
-	public String colorize(String msg) {
-		String coloredMsg = "";
-		for (int i = 0; i < msg.length(); i++) {
-			if (msg.charAt(i) == '&')
-				coloredMsg += '§';
-			else
-				coloredMsg += msg.charAt(i);
-		}
-		return coloredMsg;
-	}
 
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent e) {
 		Player p = e.getPlayer();
 		if ((!p.hasPermission("ssgminetopia.build")) && !p.isOp()) {
-			p.sendMessage(colorize(main.getConfig().getString("NoPlaceMessage")));
+			p.sendMessage(Main.cc(main.getConfig().getString("NoPlaceMessage")));
 			e.setCancelled(true);
 		}
 
