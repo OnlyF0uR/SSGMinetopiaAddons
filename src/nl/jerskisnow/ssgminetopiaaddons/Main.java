4ad41efa99bff9bf3300f1f3a2bcfa7446fe188d
Jerry Blaauw <jerskisnow@gmail.com>
2020-05-19 14:15:38 +0200

    Bug fix

--- src/nl/jerskisnow/ssgminetopiaaddons/Main.java
+++ src/nl/jerskisnow/ssgminetopiaaddons/Main.java
@@ -33,7 +33,7 @@ public class Main extends JavaPlugin {
         String nmsver = Bukkit.getServer().getClass().getPackage().getName();
         nmsver = nmsver.substring(nmsver.lastIndexOf(".") + 1);
         return !nmsver.startsWith("v1_7_") && !nmsver.startsWith("v1_8_") && !nmsver.startsWith("v1_9_")
-                && !nmsver.startsWith("v1_10_") && !nmsver.startsWith("v1_11_");
+                && !nmsver.startsWith("v1_10_") && !nmsver.startsWith("v1_11_") && !nmsver.startsWith("v1_12_");
     }
     
     private void loadCommands() {
